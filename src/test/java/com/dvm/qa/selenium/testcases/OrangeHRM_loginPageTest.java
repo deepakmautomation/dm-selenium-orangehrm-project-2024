0a0fb6c5de217b69fa4f37a38a9c6b7d9dcd9b10
Deepakm <deepakmautomation@gmail.com>
2024-07-09 21:46:29 +0530

    add testng-single.xml file to pom.xml

--- src/test/java/com/dvm/qa/selenium/testcases/OrangeHRM_loginPageTest.java
+++ src/test/java/com/dvm/qa/selenium/testcases/OrangeHRM_loginPageTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
 import com.dvm.qa.selenium.pages.OrangeHRM_dashboardPage;
 import com.dvm.qa.selenium.pages.OrangeHRM_loginPage;
 import com.dvm.qa.selenium.testbase.TestBase;
